e7afa3cb80c990f78d6dbb12b036f30ec4b94b68
frostice482 <frostice48260@gmail.com>
2025-12-27 16:15:07 +0700

    indentation

--- talisman/noanims.lua
+++ talisman/noanims.lua
@@ -1,39 +1,40 @@
 G.FUNCS.evaluate_play = function(e)
-  if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "intro" end
-  text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_intro()
-  if not G.GAME.blind:debuff_hand(G.play.cards, poker_hands, text) then
-    if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "main" end
-    text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_main(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
-  else
-    if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "debuff" end
-    text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_debuff(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
-  end
-  if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "final_scoring" end
-  text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_final_scoring(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
-  if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "after" end
-  evaluate_play_after(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
-  if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = nil end
+	if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "intro" end
+	text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_intro()
+	if not G.GAME.blind:debuff_hand(G.play.cards, poker_hands, text) then
+		if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "main" end
+		text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_main(text,disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
+	else
+		if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "debuff" end
+		text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_debuff(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
+	end
+	if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "final_scoring" end
+	text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta = evaluate_play_final_scoring(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
+	if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = "after" end
+	evaluate_play_after(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
+	if Talisman.scoring_coroutine then Talisman.scoring_coroutine.state = nil end
 end
 
 function Talisman.no_anims_calculating_misc()
-  return Talisman.config_file.disable_anims and (Talisman.current_calc.joker or Talisman.current_calc.score or Talisman.current_calc.card)
+	return Talisman.config_file.disable_anims and
+	(Talisman.current_calc.joker or Talisman.current_calc.score or Talisman.current_calc.card)
 end
 
 function Talisman.no_anims_calculating()
-  return Talisman.config_file.disable_anims and Talisman.scoring_coroutine
+	return Talisman.config_file.disable_anims and Talisman.scoring_coroutine
 end
 
 local upd = Game.update
 function Game:update(dt)
-    if Talisman.temp_dollar_update then
-      G.HUD:get_UIE_by_ID('dollar_text_UI').config.object:update()
-      G.HUD:recalculate()
-      Talisman.temp_dollar_update = false
-    end
-    if Talisman.temp_uht_config and Talisman.temp_uht_vals then
-      update_hand_text(Talisman.temp_uht_config, Talisman.temp_uht_vals)
-    end
-    return upd(self, dt)
+	if Talisman.temp_dollar_update then
+		G.HUD:get_UIE_by_ID('dollar_text_UI').config.object:update()
+		G.HUD:recalculate()
+		Talisman.temp_dollar_update = false
+	end
+	if Talisman.temp_uht_config and Talisman.temp_uht_vals then
+		update_hand_text(Talisman.temp_uht_config, Talisman.temp_uht_vals)
+	end
+	return upd(self, dt)
 end
 
 local gfep = G.FUNCS.evaluate_play
@@ -48,71 +49,71 @@ end
 --Changed this to always work; it's less pretty but fine for held in hand things
 local edo = ease_dollars
 function ease_dollars(mod, instant)
-  if not Talisman.config_file.disable_anims then return edo(mod, instant) end --and (Talisman.current_calc.joker or Talisman.current_calc.score or Talisman.current_calc.card) then
+	if not Talisman.config_file.disable_anims then return edo(mod, instant) end --and (Talisman.current_calc.joker or Talisman.current_calc.score or Talisman.current_calc.card) then
 
-  mod = mod or 0
-  if to_big(mod) > BigC.ZERO then inc_career_stat('c_dollars_earned', mod) end
-  G.GAME.dollars = G.GAME.dollars + mod
-  Talisman.temp_dollar_update = true
+	mod = mod or 0
+	if to_big(mod) > BigC.ZERO then inc_career_stat('c_dollars_earned', mod) end
+	G.GAME.dollars = G.GAME.dollars + mod
+	Talisman.temp_dollar_update = true
 end
 
 local sm = Card.start_materialize
-function Card:start_materialize(a,b,c)
-    if Talisman.no_anims_calculating_misc() then return end
-    return sm(self,a,b,c)
+function Card:start_materialize(a, b, c)
+	if Talisman.no_anims_calculating_misc() then return end
+	return sm(self, a, b, c)
 end
 
 local sd = Card.start_dissolve
-function Card:start_dissolve(a,b,c,d)
-    if Talisman.no_anims_calculating_misc() then return self:remove() end
-    return sd(self,a,b,c,d)
+function Card:start_dissolve(a, b, c, d)
+	if Talisman.no_anims_calculating_misc() then return self:remove() end
+	return sd(self, a, b, c, d)
 end
 
 local ss = Card.set_seal
-function Card:set_seal(a,b,immediate)
-    return ss(self,a,b,immediate or Talisman.no_anims_calculating_misc())
+function Card:set_seal(a, b, immediate)
+	return ss(self, a, b, immediate or Talisman.no_anims_calculating_misc())
 end
 
 local cest = card_eval_status_text
-function card_eval_status_text(a,b,c,d,e,f)
-    if not Talisman.config_file.disable_anims then return cest(a,b,c,d,e,f) end
+function card_eval_status_text(a, b, c, d, e, f)
+	if not Talisman.config_file.disable_anims then return cest(a, b, c, d, e, f) end
 end
 
 local jc = juice_card
 function juice_card(x)
-    if not Talisman.config_file.disable_anims then return jc(x) end
+	if not Talisman.config_file.disable_anims then return jc(x) end
 end
 
 local cju = Card.juice_up
 function Card:juice_up(...)
-    if not Talisman.config_file.disable_anims then return cju(self, ...) end
+	if not Talisman.config_file.disable_anims then return cju(self, ...) end
 end
 
 local uht = update_hand_text
 function update_hand_text(config, vals)
-    if config.immediate or not (Talisman.config_file.disable_anims and Talisman.scoring_coroutine) then
-      Talisman.temp_uht_config = nil
-      Talisman.temp_uht_vals = nil
-      return uht(config, vals)
-    end
+	if config.immediate or not (Talisman.config_file.disable_anims and Talisman.scoring_coroutine) then
+		Talisman.temp_uht_config = nil
+		Talisman.temp_uht_vals = nil
+		return uht(config, vals)
+	end
 
-    Talisman.temp_uht_config = config
-    Talisman.temp_uht_config.immediate = true
-    Talisman.temp_uht_vals = vals
+	Talisman.temp_uht_config = config
+	Talisman.temp_uht_config.immediate = true
+	Talisman.temp_uht_vals = vals
 end
 
 local gfer = G.FUNCS.evaluate_round
 function G.FUNCS.evaluate_round()
-  if not Talisman.config_file.disable_anims then return gfer() end
+	if not Talisman.config_file.disable_anims then return gfer() end
 
-  if to_big(G.GAME.chips) >= to_big(G.GAME.blind.chips) then
-      add_round_eval_row({dollars = G.GAME.blind.dollars, name='blind1', pitch = 0.95})
-  else
-      add_round_eval_row({dollars = 0, name='blind1', pitch = 0.95, saved = true})
-  end
-  local arer = add_round_eval_row
-  add_round_eval_row = function() return end
-  local dollars = gfer()
-  add_round_eval_row = arer
-  add_round_eval_row({name = 'bottom', dollars = Talisman.dollars})
+	if to_big(G.GAME.chips) >= to_big(G.GAME.blind.chips) then
+		add_round_eval_row({ dollars = G.GAME.blind.dollars, name = 'blind1', pitch = 0.95 })
+	else
+		add_round_eval_row({ dollars = 0, name = 'blind1', pitch = 0.95, saved = true })
+	end
+	local arer = add_round_eval_row
+	add_round_eval_row = function() return end
+	local dollars = gfer()
+	add_round_eval_row = arer
+	add_round_eval_row({ name = 'bottom', dollars = Talisman.dollars })
 end
--- talisman/safety.lua
+++ talisman/safety.lua
@@ -1,55 +1,55 @@
 function Talisman.sanitize(obj, done)
-  if not done then done = {} end
-  if done[obj] then return obj end
-  done[obj] = true
+	if not done then done = {} end
+	if done[obj] then return obj end
+	done[obj] = true
 
-  for k,v in pairs(obj) do
-    local t = type(v)
-    if Big and Big.is(v) then
-      obj[k] = v:as_table()
-    elseif t == "table" then
-      Talisman.sanitize(v, done)
-    end
-  end
+	for k,v in pairs(obj) do
+		local t = type(v)
+		if Big and Big.is(v) then
+			obj[k] = v:as_table()
+		elseif t == "table" then
+			Talisman.sanitize(v, done)
+		end
+	end
 
-  return obj
+	return obj
 end
 
 function Talisman.create_unpack_env()
-  return {
-    Big = Big,
-    OmegaMeta = OmegaMeta,
-    to_big = to_big,
-    uncompress_big = uncompress_big,
-    inf = 1.79769e308,
-  }
+	return {
+		Big = Big,
+		OmegaMeta = OmegaMeta,
+		to_big = to_big,
+		uncompress_big = uncompress_big,
+		inf = 1.79769e308,
+	}
 end
 
 function Talisman.copy_table(obj, reflist)
-  if not reflist then reflist = {} end
-  if type(obj) ~= 'table' then return obj end
-  if Big and Big.is(obj) then return obj end
-  if reflist[obj] then return reflist[obj] end
+	if not reflist then reflist = {} end
+	if type(obj) ~= 'table' then return obj end
+	if Big and Big.is(obj) then return obj end
+	if reflist[obj] then return reflist[obj] end
 
-  local copy = {}
-  reflist[obj] = copy
-  for k, v in pairs(obj) do
-      copy[Talisman.copy_table(k, reflist)] = Talisman.copy_table(v, reflist)
-  end
-  setmetatable(copy, Talisman.copy_table(getmetatable(obj), reflist))
+	local copy = {}
+	reflist[obj] = copy
+	for k, v in pairs(obj) do
+		copy[Talisman.copy_table(k, reflist)] = Talisman.copy_table(v, reflist)
+	end
+	setmetatable(copy, Talisman.copy_table(getmetatable(obj), reflist))
 
-  return copy
+	return copy
 end
 
 local copy_table_hook = copy_table
 function copy_table(v)
-  if not Talisman.config_file.enable_compat or not Big then return copy_table_hook(v) end
-  return Talisman.copy_table(v)
+	if not Talisman.config_file.enable_compat or not Big then return copy_table_hook(v) end
+	return Talisman.copy_table(v)
 end
 
 function STR_UNPACK(str)
-  local env = Talisman.create_unpack_env()
-  local chunk = assert(load(str, '=[temp:str_unpack]', 'bt', env))
-  setfenv(chunk, env)
-  return chunk()
+	local env = Talisman.create_unpack_env()
+	local chunk = assert(load(str, '=[temp:str_unpack]', 'bt', env))
+	setfenv(chunk, env)
+	return chunk()
 end
