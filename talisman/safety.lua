8179b570cde8e7cf0dbcaf46d0a0403de41da994
frostice482 <frostice48260@gmail.com>
2026-01-23 07:28:38 +0700

    screw this

--- talisman/safety.lua
+++ talisman/safety.lua
@@ -53,3 +53,51 @@ function STR_UNPACK(str)
 	setfenv(chunk, env)
 	return chunk()
 end
+
+-- scan for non-transferable objects for Channel.push
+-- for easier debugging
+
+local reg = debug.getregistry()
+local Channel = reg.Channel
+local _push = Channel.push
+
+local stack = {}
+local refs = {}
+local testch = love.thread.newChannel()
+
+local function scantransferable(val, i)
+	local t = type(val)
+	if t == "nil" or t == "number" or t == "string" or t == "boolean" then
+		return
+	end
+	if t == "userdata" then
+		if not pcall(_push, testch, val) then
+			error(string.format("%s: userdata is not transferable", table.concat(stack, '.', 1, i)))
+		end
+		testch:clear()
+		return
+	end
+	if t == "table" then
+		if refs[val] then
+			error(string.format("%s: cyclic", table.concat(stack, '.', 1, i)))
+		end
+
+		refs[val] = true
+		for k,v in pairs(val) do
+			stack[i+1] = tostring(k)
+			scantransferable(k, i+1)
+			scantransferable(v, i+1)
+		end
+		stack[i+1] = nil
+		refs[val] = nil
+		return
+	end
+
+	error(string.format("%s: type %s is not transferable", table.concat(stack, '.', 1, i), t))
+end
+
+function Channel:push(obj)
+	refs = {}
+	scantransferable(obj, 0)
+	return _push(self, obj)
+end
