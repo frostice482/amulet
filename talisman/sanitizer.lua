8c60c8b6f3554b4182a7202e5e00d09e2e3272f8
frostice482 <frostice48260@gmail.com>
2026-01-31 06:23:19 +0700

    rename safety to sanitizer

--- talisman/sanitizer.lua
+++ talisman/sanitizer.lua
@@ -1,4 +1,7 @@
-function Talisman.sanitize(obj, tonum, done)
+Talisman.sanitizer = {}
+local sanitizer = Talisman.sanitizer
+
+function sanitizer.sanitize(obj, tonum, done)
 	if not Big then return end
 	if Big.is(obj) then
 		return tonum and obj.number or obj:as_table()
@@ -15,16 +18,16 @@ function Talisman.sanitize(obj, tonum, done)
 			k = tonum and k.number or k:as_table()
 			obj[k] = v
 		else
-			Talisman.sanitize(k, tonum, done)
+			sanitizer.sanitize(k, tonum, done)
 		end
 
-		obj[k] = Talisman.sanitize(v, tonum, done)
+		obj[k] = sanitizer.sanitize(v, tonum, done)
 	end
 
 	return obj
 end
 
-function Talisman.create_unpack_env()
+function sanitizer.create_unpack_env()
 	return {
 		Big = Big,
 		OmegaMeta = OmegaMeta,
@@ -42,7 +45,7 @@ end
 --- @param obj any
 --- @param config? t.CopyTableConfig
 --- @param reflist any
-function Talisman.copy_table(obj, config, reflist)
+function sanitizer.copy_table(obj, config, reflist)
 	if Big and Big.is(obj) then
 		if not (config and config.sanitize) then return obj end
 		if config.sanitizeToNumber then return obj.number end
@@ -56,23 +59,23 @@ function Talisman.copy_table(obj, config, reflist)
 	reflist[obj] = copy
 
 	for k, v in pairs(obj) do
-		copy[Talisman.copy_table(k, config, reflist)] = Talisman.copy_table(v, config, reflist)
+		copy[sanitizer.copy_table(k, config, reflist)] = sanitizer.copy_table(v, config, reflist)
 	end
 	if not (config and config.nometa) then
-		setmetatable(copy, Talisman.copy_table(getmetatable(obj), config, reflist))
+		setmetatable(copy, sanitizer.copy_table(getmetatable(obj), config, reflist))
 	end
 
 	return copy
 end
 
 --- @type t.CopyTableConfig
-Talisman.copy_for_thread = {
+sanitizer.copy_for_thread = {
 	sanitize = true,
 	nometa = true
 }
 
 --- @type t.CopyTableConfig
-Talisman.copy_for_thread_num = {
+sanitizer.copy_for_thread_num = {
 	sanitize = true,
 	sanitizeToNumber = true,
 	nometa = true
@@ -81,11 +84,11 @@ Talisman.copy_for_thread_num = {
 local copy_table_hook = copy_table
 function copy_table(v)
 	if not Talisman.config_file.enable_compat or not Big then return copy_table_hook(v) end
-	return Talisman.copy_table(v)
+	return sanitizer.copy_table(v)
 end
 
 function STR_UNPACK(str)
-	local env = Talisman.create_unpack_env()
+	local env = sanitizer.create_unpack_env()
 	local chunk = assert(load(str, '=[temp:str_unpack]', 'bt', env))
 	setfenv(chunk, env)
 	return chunk()
@@ -97,9 +100,9 @@ local _push = Channel.push
 
 function Channel:push(obj)
 	if Talisman.config_file.thread_sanitize == "copy" then
-		obj = Talisman.copy_table(obj, Talisman.config_file.thread_sanitize_num and Talisman.copy_for_thread_num or Talisman.copy_for_thread)
+		obj = sanitizer.copy_table(obj, Talisman.config_file.thread_sanitize_num and sanitizer.copy_for_thread_num or sanitizer.copy_for_thread)
 	elseif Talisman.config_file.thread_sanitize == "modify" then
-		obj = Talisman.sanitize(obj, Talisman.config_file.thread_sanitize_num)
+		obj = sanitizer.sanitize(obj, Talisman.config_file.thread_sanitize_num)
 	end
 	return _push(self, obj)
 end
