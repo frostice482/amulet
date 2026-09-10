58f19c52251dd3856bcbf23b4ee420a0ab51fffc
frostice482 <frostice48260@gmail.com>
2026-09-10 13:49:20 +0700

    dynamic gradient registry

--- talisman/smods/gradients.lua
+++ talisman/smods/gradients.lua
@@ -1,32 +1,17 @@
 local gradlist = Talisman.gradients.list
 
-SMODS.Gradient {
-    key = "echips",
-    colours = {
-        HEX("41bed9"),
-        HEX("5674e9"),
-    },
-    cycle = 4,
-    update = function(self, dt)
-        for i = 1, 4 do
-            self[i] = gradlist.echips.current_colour[i]
-        end
-    end,
-}
-
-SMODS.Gradient {
-    key = "emult",
-    colours = {
-        HEX("ff73ad"),
-        HEX("db005f")
-    },
-    cycle = 4,
-    update = function(self, dt)
-        for i = 1, 4 do
-            self[i] = gradlist.emult.current_colour[i]
-        end
-    end,
-}
+for k,v in pairs(gradlist) do
+    SMODS.Gradient {
+        key = v.key,
+        colours = v.colours,
+        cycle = 4,
+        update = function(self, dt)
+            for i = 1, 4 do
+                self[i] = v.current_colour[i]
+            end
+        end,
+    }
+end
 
 if Spectrallib then
     local update_exp_colours_ref = G.FUNCS.slib_update_exp_colours
