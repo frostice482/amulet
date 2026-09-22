0db882da14cb6a146f26ca3b640e7771fc0c31f7
frostice482 <frostice48260@gmail.com>
2026-09-23 00:22:13 +0700

    r2mm make me do this

--- talisman.lua
+++ talisman.lua
@@ -6,6 +6,28 @@ local talisman_path = _mod_dir_amulet
 assert(tinymount(talisman_path .. '/talisman', 'talisman', 0) ~= 0, 'Amulet: Failed to mount talisman from ' .. talisman_path)
 assert(tinymount(talisman_path .. '/big-num', 'big-num', 0) ~= 0, 'Amulet: Failed to mount big-num from ' .. talisman_path)
 
+local tal_ok, tal_c_err = pcall(require, "talisman.configinit")
+if not tal_ok then
+local msg = [[
+
+Amulet failed to load (sanity check failed). You might want to check your installation.
+Debug info:
+ - amulet folder: %s
+ - mods folder: %s
+ - stat.type for talisman folder: %s
+ - directory listing for talisman folder: [%s]
+
+%s
+]]
+error(msg:format(
+    _mod_dir_amulet,
+    require("lovely").mod_dir,
+    love.filesystem.getInfo("talisman") and love.filesystem.getInfo("talisman").type or "?",
+    table.concat(love.filesystem.getDirectoryItems("talisman"), '; '),
+    tal_c_err
+), 0)
+end
+
 require("talisman.configinit")
 require("talisman.effects")
 require("talisman.globals")
